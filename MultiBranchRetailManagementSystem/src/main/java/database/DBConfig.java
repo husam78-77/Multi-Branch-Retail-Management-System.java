acd6d0751b5c4d5884b58bd23cb3d42d062de966
husam78-77 <husamprince78@gmail.com>
2025-12-26 16:09:48 +0800

    full progress/no validation

--- MultiBranchRetailManagementSystem/src/main/java/database/DBConfig.java
+++ MultiBranchRetailManagementSystem/src/main/java/database/DBConfig.java
@@ -2,8 +2,11 @@ package database;
 
 public class DBConfig {
 
-    public static final String URL =
-        "jdbc:postgresql://db.wjgttszgfgjaodgbqhul.supabase.co:5432/postgres";
+	public static final String URL =
+		    "jdbc:postgresql://db.wjgttszgfgjaodgbqhul.supabase.co:5432/postgres"
+		  + "?sslmode=require"
+		  + "&sslrootcert=disable";
+
 
     public static final String USER = "postgres";
     public static final String PASSWORD = "HUSSam7575!";
